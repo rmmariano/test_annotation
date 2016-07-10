7a7ef1c383888961df20334d582eb8cbfac83792
Rodrigo M. Mariano <rodmariano13@gmail.com>
2016-07-10 17:14:23 -0300

    Adding example how to use the invoke

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Main {
@@ -10,17 +11,20 @@ public class Main {
 //		The class that you want to get the metainformation
 	    Class<SomeClass> classe = SomeClass.class;
 	    
+	    
 	    System.out.println("\n- List of Fields:");
 	    Field[] fields = classe.getDeclaredFields();
 		for (Field atributo: fields) {
 			System.out.println(atributo.getName());      
 		}
 		
+		
 		System.out.println("\n- List of Methods:");
 		Method[] methods = classe.getMethods();
 		for (Method metodo: methods) {
 			System.out.println(metodo.getName());      
 		}
+		
 		
 		System.out.println("\n- List of Methods with the Info's annotation:");
 		methods = classe.getMethods();
@@ -49,6 +53,27 @@ public class Main {
 //			}	
 	    
 		}
+		
+		
+		System.out.println("\n- Executing the List of Methods with the Info's annotation:");
+		SomeClass obj = new SomeClass();
+		
+		methods = classe.getMethods();
+		for (Method metodo: methods) {				
+			Info info = metodo.getAnnotation(Info.class);
+			
+			if(info != null){
+				try {					
+					System.out.println(metodo.getName());					
+					// Execute the method "metodo" of the object "obj", something like this: "obj.metodo()"
+					System.out.println(metodo.invoke(obj));						
+				} catch (Exception e) {
+					e.printStackTrace();
+				}				
+			}
+			
+		}
+		
 
 	}
 }
